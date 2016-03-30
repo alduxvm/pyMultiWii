704a03539d981929a7088e2998c9e833f2097145
Aldo Vargas <alduxvm@gmail.com>
2016-03-30 18:12:07 +0100

    Adding the MOTOR command

--- test-attitude.py
+++ test-attitude.py
@@ -16,8 +16,8 @@ from sys import stdout
 
 if __name__ == "__main__":
 
-    board = MultiWii("/dev/ttyUSB0")
-    #board = MultiWii("/dev/tty.SLAB_USBtoUART")
+    #board = MultiWii("/dev/ttyUSB0")
+    board = MultiWii("/dev/tty.SLAB_USBtoUART")
     try:
         while True:
             board.getData(MultiWii.ATTITUDE)
