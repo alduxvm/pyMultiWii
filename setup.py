ac53ba6874d58da7245fa3f11801b226197bb5eb
Wil <wfkoch@gmail.com>
2017-06-22 16:46:13 -0400

    All installation through pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pymultiwii",
+    description="MultiWii Serial Protocol (MSP) API to send and receive data from firmware using MSP",
+    version="1.6",
+    url="https://github.com/alduxvm/pyMultiWii",
+    author="Aldo Vargas",
+    license="GPL",
+    packages=['pymultiwii'],
+
+
+)
